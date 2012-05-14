c4e74acca01ed68ce49151e430a93be4e49237f9
Robert Clipsham <robert@octarineparrot.com>
2012-05-14 15:42:56 +0100

    Support custom amounts of memory.
    Still defaults to 30000, but --memory can be used to specify another amount.
    Also added a --help switch and some command line parsing.

--- bf.c
+++ bf.c
@@ -16,17 +16,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-char array[30000];
-char *p = array;
+char *array;
+int array_length = 30000;
+char *p;
 
 char *bf;
-char *bfStart;
-size_t bfLength;
+char *bf_start;
+size_t bf_length;
 
 void get_location(size_t *line, size_t *character)
 {
     *line = 1;
-    char *cursor = bfStart;
+    char *cursor = bf_start;
     char *last_line = cursor;
     for (; cursor < bf; cursor++)
     {
@@ -39,11 +40,20 @@ void get_location(size_t *line, size_t *character)
     *character = bf - last_line;
 }
 
-void error(const char *msg)
+void error_aux(const char *msg, const char *aux)
 {
     size_t line, character;
     get_location(&line, &character);
     fprintf(stderr, "error(%zu, %zu): %s\n", line, character, msg);
+    if (aux != NULL)
+    {
+        fprintf(stderr, "              %s\n", aux);
+    }
+}
+
+void error(const char *msg)
+{
+    error_aux(msg, NULL);
 }
 
 int act(char c)
@@ -57,9 +67,11 @@ int act(char c)
     switch(c)
     {
         case '>':
-            if (p + 1 >= array + 30000)
+            if (p + 1 >= array + array_length)
             {
-                error("overflow at char >");
+                char *try;
+                sprintf(try, "try using --memory with a value greater than %d", array_length);
+                error_aux("overflow at char >", try);
                 return 1;
             }
             ++p;
@@ -122,8 +134,8 @@ int read_file(const char *f)
         return 1;
     }
     fseek(file, 0, SEEK_END);
-    bfLength = ftell(file);
-    bf = malloc(bfLength + 1);
+    bf_length = ftell(file);
+    bf = malloc(bf_length + 1);
     if (bf == NULL)
     {
         fprintf(stderr, "Memory error\n");
@@ -131,27 +143,67 @@ int read_file(const char *f)
     }
     rewind(file);
 
-    if (fread(bf, 1, bfLength, file) != bfLength)
+    if (fread(bf, 1, bf_length, file) != bf_length)
     {
         perror("Unable to read file");
         return 1;
     }
     fclose(file);
-    bf[bfLength] = '\0';
-    bfStart = bf;
+    bf[bf_length] = '\0';
+    bf_start = bf;
+    return 0;
+}
+
+int show_help(const char *program_name)
+{
+    fprintf(stderr, "Usage: %s [options] <file>\n", program_name);
+    fprintf(stderr, "Options:\n");
+    fprintf(stderr, "   --help              Print this help message\n");
+    fprintf(stderr, "   --memory=<number>   Memory to allocate. Defaults to 30000.\n");
+    return 1;
+}
+
+int alloc_memory(void)
+{
+    array = malloc(array_length);
+    if (array == NULL)
+    {
+        fprintf(stderr, "Memory error\n");
+        return 1;
+    }
+    p = array;
     return 0;
 }
 
 int main(int argc, const char *argv[])
 {
-    if (argc != 2)
+    if (argc > 3 || argc < 2)
+        return show_help(argv[0]);
+    
+    if (argc >= 2)
     {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        return 1;
+        if (strcmp(argv[1], "--help") == 0) return show_help(argv[0]);
+        else if (strncmp(argv[1], "--memory=", 9) == 0)
+        {
+            if (argc != 3)
+            {
+                return show_help(argv[0]);
+            }
+            int memory = atoi(argv[1] + 9);
+            if (memory < 1)
+            {
+                fprintf(stderr, "error: available memory must be > 0\n");
+                return 1;
+            }
+            array_length = memory;
+        }
     }
-    if (read_file(argv[1]))
+
+    if (alloc_memory()) return 1;
+
+    if (read_file(argv[argc == 2 ? 1 : 2]))
         return 1;
-    for (size_t i = 0; i < bfLength; i++)
+    for (size_t i = 0; i < bf_length; i++)
     {
         if (act(*bf++))
             return 1;
